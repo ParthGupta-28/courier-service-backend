c10bcb8f3bdb8ab630045d4733ad0e810fbb117c
ParthGupta-28 <pg6272695@gmail.com>
2023-05-11 01:38:23 +0530

    try6

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ ENV PORT 8080
 
 COPY courier-service-backend-0.0.1-SNAPSHOT.jar .
 
-ENTRYPOINT ["java","-jar","courier-service-backend-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java","-jar","-Djasypt.encryptor.password=courierbackend","courier-service-backend-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/courierservice/courierservicebackend/CourierServiceBackendApplication.java
+++ src/main/java/com/courierservice/courierservicebackend/CourierServiceBackendApplication.java
@@ -3,8 +3,11 @@ package com.courierservice.courierservicebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class CourierServiceBackendApplication {
 
 	public static void main(String[] args) {
