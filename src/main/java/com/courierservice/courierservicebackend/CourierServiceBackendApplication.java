7705ad3b3b535d4237b16fd191c287f1753f38fe
ParthGupta-28 <pg6272695@gmail.com>
2023-05-15 21:20:38 +0530

    done

--- src/main/java/com/courierservice/courierservicebackend/CourierServiceBackendApplication.java
+++ src/main/java/com/courierservice/courierservicebackend/CourierServiceBackendApplication.java
@@ -3,11 +3,8 @@ package com.courierservice.courierservicebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class CourierServiceBackendApplication {
 
 	public static void main(String[] args) {
